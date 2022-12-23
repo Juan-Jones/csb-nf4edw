f54d081e8dbaf815ea320605ccc41b2d792a6ecc
Code Sandbox <hello@codesandbox.io>
2022-12-23 01:29:44 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8eface850f405432a0153e748d9a7a77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4a03538622c8512f8949921023dd4d",
+    "url": "https://juan-jones.github.io/csb-nf4edw/index.html"
+  },
+  {
+    "revision": "51be5fa457bed8c0f751",
+    "url": "https://juan-jones.github.io/csb-nf4edw/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "a392f5839c7b5aaa864e",
+    "url": "https://juan-jones.github.io/csb-nf4edw/static/css/main.6452e301.chunk.css"
+  },
+  {
+    "revision": "51be5fa457bed8c0f751",
+    "url": "https://juan-jones.github.io/csb-nf4edw/static/js/2.1eaf9732.chunk.js"
+  },
+  {
+    "revision": "90f64883d8387b00b02a17935a4dad27",
+    "url": "https://juan-jones.github.io/csb-nf4edw/static/js/2.1eaf9732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a392f5839c7b5aaa864e",
+    "url": "https://juan-jones.github.io/csb-nf4edw/static/js/main.6a2ee086.chunk.js"
+  },
+  {
+    "revision": "6d00c1b8c87525bbc798",
+    "url": "https://juan-jones.github.io/csb-nf4edw/static/js/runtime-main.cf99ee20.js"
+  }
+]);
